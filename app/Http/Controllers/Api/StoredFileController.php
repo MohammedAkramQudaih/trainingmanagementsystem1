5aab3d5a613c498dcd1cf1f70be457a8ebc732eb
Ahmed Hammad <ashammad99@gnail.com>
2023-05-24 11:55:01 +0300

    second Commit

---
+++ app/Http/Controllers/Api/StoredFileController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\StoredFile;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class StoredFileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //no need to it
+        return response()->json([
+            'message' => 'listing all stored Files'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'fileName' => 'required|string',
+            'fileUrl' => 'required|string|unique:stored_files,fileUrl',
+            'fileType' => 'required|string',
+            'fileSize' => 'required|integer',
+            'user_id' => 'required|integer|exists:users,id'
+        ]);
+        $storedFile = new StoredFile();
+        $storedFile->fileName = $request->input('fileName');
+        $storedFile->fileUrl = $request->input('fileUrl');
+        $storedFile->fileType = $request->input('fileType');
+        $storedFile->fileSize = $request->input('fileSize');
+        $storedFile->user_id = $request->input('user_id');
+        $storedFile->save();
+        return response()->json([
+            'File' => $storedFile,
+            'message' => 'The File Uploaded Successfully'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $userId)
+    {
+        $user = User::find($userId);
+        $files = $user->storedFiles;
+        return response()->json($files, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $file = StoredFile::withoutTrashed()->find($id);
+        $file->delete();
+        return response()->json([
+            'message' => 'The File deleted Successfully'
+        ]);
+    }
+}
---
+++ app/Models/StoredFile.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class StoredFile extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'fileName',
+        'fileType',
+        'fileSize',
+        'user_id',
+        'fileUrl'
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    protected $hidden = [
+        'created_at', 'updated_at', 'deleted_at'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
